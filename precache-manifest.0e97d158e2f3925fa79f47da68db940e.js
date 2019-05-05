750b932c825573bf2dfbbdc6e9e06b55ced810b6
przemo <podkowa82@gmail.com>
2019-05-05 22:38:54 +0200

    Updates

---
+++ precache-manifest.0e97d158e2f3925fa79f47da68db940e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e001307ba7b4d3de531ae298c23bc7",
+    "url": "/react-snake-game/index.html"
+  },
+  {
+    "revision": "95aa9821f90e9da8d978",
+    "url": "/react-snake-game/static/css/main.aa3b6223.chunk.css"
+  },
+  {
+    "revision": "3735ccaef9ac92ca05c6",
+    "url": "/react-snake-game/static/js/2.2b788eca.chunk.js"
+  },
+  {
+    "revision": "95aa9821f90e9da8d978",
+    "url": "/react-snake-game/static/js/main.4e803ddd.chunk.js"
+  },
+  {
+    "revision": "2bb4e33f074ce70d55a3",
+    "url": "/react-snake-game/static/js/runtime~main.87448e3a.js"
+  }
+]);
